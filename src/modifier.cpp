d29f47e2af174d936e4805e7d26e6f587e2efd78
coco875 <pereira.jannin@gmail.com>
2025-03-28 19:03:33 +0100

    multiple change
    * rename math_helper to math_utils
    * add matrix function
    * some refactor in math_utils

--- src/modifier.cpp
+++ src/modifier.cpp
@@ -2,37 +2,22 @@
 
 #include <common.h>
 
-cv::Mat translate(float x, float y, cv::Mat mat) {
-    cv::Mat out = mat.clone();
-    out.at<float>(0, 2) += x;
-    out.at<float>(1, 2) += y;
-    return out;
-}
-
-void print_mat(cv::Mat mat) {
-    for (int i = 0; i < mat.rows; i++) {
-        for (int j = 0; j < mat.cols; j++) {
-            std::cout << mat.at<float>(i, j) << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
+#include "utils/math_utils.h"
 
 int main(int argc, char const* argv[]) {
-    // if (argc < 2) {
-    //     std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
-    //     return 1;
-    // }
-    // std::string image_path = argv[1];
-    std::string image_path = "../copies/copy.png";
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
+        return 1;
+    }
+    std::string image_path = argv[1];
     cv::Mat img = cv::imread(image_path);
     cv::Mat calibrated_img = img.clone();
-    cv::Mat identity = cv::Mat::eye(2, 3, CV_32F);
+    cv::Mat identity = cv::Mat::eye(3, 3, CV_32F);
+    identity *= rotate_center(45, img.cols / 2, img.rows / 2);
     print_mat(identity);
-    identity = cv::getRotationMatrix2D(cv::Point2f(img.cols / 2, img.rows / 2), 0.0, 1.0);
+    identity *= translate(0, 0);
     print_mat(identity);
-    identity = translate(100, 100, identity);
+    identity = identity(cv::Rect(0, 0, 3, 2));
     print_mat(identity);
     cv::warpAffine(img, calibrated_img, identity, calibrated_img.size(), cv::INTER_LINEAR);
     cv::imwrite("calibrated_img.png", calibrated_img);
