76cd2650c3a4175ae0f20a64c02cab3e8f6b1597
enzoodac <enzo.da-cunha@univ-tlse3.fr>
2025-04-14 20:34:11 +0200

    salt_pepper
    salt_pepper and gaussian noises added
    only salt_pepper works atm

--- src/modifier.cpp
+++ src/modifier.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
 
+#include <random>
 #include <common.h>
 
 #include "utils/math_utils.h"
 
-int main(int argc, char const* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
+// void add_salt_pepper_noise(float noise_ratio) 
+// {
+   
+// }   
+
+void add_salt_pepper_noise(cv::Mat &srcArr, float max_pepper, float max_salt)
+
+{   cv::RNG rng; 
+    int amount1=srcArr.rows*srcArr.cols*max_pepper;
+    int amount2=srcArr.rows*srcArr.cols*max_salt;
+    for(int counter=0; counter<amount1; ++counter)
+    {
+      srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0, srcArr.cols)) = cv::Vec3b(0, 0, 0);
+    }
+     for (int counter=0; counter<amount2; ++counter)
+     {
+     srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0,srcArr.cols)) = cv::Vec3b(255, 255, 255);
+     }
+}
+
+
+void Add_gaussian_Noise(cv::Mat &srcArr,double mean,double sigma)       //TO DO
+{
+    cv::Mat NoiseArr = srcArr.clone();
+    cv::RNG rng;
+    rng.fill(NoiseArr, cv::RNG::NORMAL, mean,sigma);  
+
+     
+    add(srcArr, NoiseArr, srcArr);   
+}
+
+
+
+int main(int argc, char const* argv[]) 
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <image_max_pepperth>" << std::endl;
         return 1;
     }
-    std::string image_path = argv[1];
-    cv::Mat img = cv::imread(image_path);
+    std::string image_max_pepperth = argv[1];
+    cv::Mat img = cv::imread(image_max_pepperth);
     cv::Mat calibrated_img = img.clone();
     cv::Mat identity = cv::Mat::eye(3, 3, CV_32F);
-    identity *= rotate_center(45, img.cols / 2, img.rows / 2);
-    print_mat(identity);
-    identity *= translate(0, 0);
-    print_mat(identity);
+    identity *= rotate_center(5, img.cols / 2, img.rows / 2);
+    //print_mat(identity);
+    identity *= translate(3, 0);
+    //print_mat(identity);
     identity = identity(cv::Rect(0, 0, 3, 2));
-    print_mat(identity);
-    cv::warpAffine(img, calibrated_img, identity, calibrated_img.size(), cv::INTER_LINEAR);
-    cv::imwrite("calibrated_img.png", calibrated_img);
+    //print_mat(identity);
+    cv::Mat noisy_img = img.clone();
+    add_salt_pepper_noise(noisy_img, 0.01f, 0.01f);  // Ajoute le bruit
+    cv::warpAffine(noisy_img, calibrated_img, identity, noisy_img.size(), cv::INTER_LINEAR);
+    cv::imwrite("calibrated_img.png",calibrated_img);
     return 0;
 }
