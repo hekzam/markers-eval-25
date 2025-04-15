6f59e827815d8d54ed59465332a441622034d8de
enzoodac <enzo.da-cunha@univ-tlse3.fr>
2025-04-15 15:44:01 +0200

    gaussian_noise
    salt_pepper and gaussian ok

--- src/modifier.cpp
+++ src/modifier.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-
-#include <random>
 #include <common.h>
-
 #include "utils/math_utils.h"
 
-// void add_salt_pepper_noise(float noise_ratio) 
-// {
-   
-// }   
 
 void add_salt_pepper_noise(cv::Mat &srcArr, float max_pepper, float max_salt)
-
-{   cv::RNG rng; 
+{   
+    cv::RNG rng; 
     int amount1=srcArr.rows*srcArr.cols*max_pepper;
     int amount2=srcArr.rows*srcArr.cols*max_salt;
     for(int counter=0; counter<amount1; ++counter)
     {
-      srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0, srcArr.cols)) = cv::Vec3b(0, 0, 0);
+        srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0, srcArr.cols)) = cv::Vec3b(0, 0, 0);
     }
-     for (int counter=0; counter<amount2; ++counter)
-     {
-     srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0,srcArr.cols)) = cv::Vec3b(255, 255, 255);
-     }
+    for (int counter=0; counter<amount2; ++counter){
+        srcArr.at<cv::Vec3b>(rng.uniform(0,srcArr.rows), rng.uniform(0,srcArr.cols)) = cv::Vec3b(255, 255, 255);
+    }
 }
 
 
-void Add_gaussian_Noise(cv::Mat &srcArr,double mean,double sigma)       //TO DO
+void add_gaussian_noise(cv::Mat &srcArr, double mean, double sigma)       
 {
     cv::Mat NoiseArr = srcArr.clone();
     cv::RNG rng;
     rng.fill(NoiseArr, cv::RNG::NORMAL, mean,sigma);  
-
-     
     add(srcArr, NoiseArr, srcArr);   
 }
+
+void brightness_modifier(c::)       //TODO
 
 
 
@@ -56,7 +48,7 @@ int main(int argc, char const* argv[])
     identity = identity(cv::Rect(0, 0, 3, 2));
     //print_mat(identity);
     cv::Mat noisy_img = img.clone();
-    add_salt_pepper_noise(noisy_img, 0.01f, 0.01f);  // Ajoute le bruit
+    add_gaussian_noise(noisy_img, 50, 50);  // Ajoute le bruit
     cv::warpAffine(noisy_img, calibrated_img, identity, noisy_img.size(), cv::INTER_LINEAR);
     cv::imwrite("calibrated_img.png",calibrated_img);
     return 0;
