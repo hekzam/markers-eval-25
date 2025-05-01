6f3f44754c605ef88420d587911aa1d0d3afe576
enzoodac <enzo.da-cunha@univ-tlse3.fr>
2025-05-01 23:28:18 +0200

    Update modifier.cpp

--- src/modifier.cpp
+++ src/modifier.cpp
@@ -12,6 +12,9 @@ cv::Mat img;
 cv::Mat noisy_img;
 int nb_appl = 0;
 
+#define MIN_ROTATE -5
+#define MAX_ROTATE 5
+
 /**
  * @brief Ajout de bruit poivre et sel
  *
@@ -113,8 +116,7 @@ void ajouterTaches(cv::Mat& image, cv::RNG rng, int nombreTaches, int rayonMin, 
     }
 }
 
-void rotate_img(int deg) 
-{
+void rotate_img(int deg) {
     cv::Mat img_out = img.clone();
     cv::Mat identity = cv::Mat::eye(3, 3, CV_32F);
     identity *= rotate_center(deg, img_out.cols / 2, img_out.rows / 2);
@@ -122,8 +124,7 @@ void rotate_img(int deg)
     cv::warpAffine(img_out, img, identity, img.size(), cv::INTER_LINEAR);
 }
 
-void translate_img(int dx, int dy) 
-{
+void translate_img(int dx, int dy) {
     cv::Mat img_out = img.clone();
     cv::Mat affine = (cv::Mat_<float>(2, 3) << 1, 0, dx, 0, 1, dy);
     cv::warpAffine(img_out, img, affine, img.size(), cv::INTER_LINEAR);
@@ -180,84 +181,83 @@ void gestion_arg(int argc, char const* argv[]) {
             }
         }
     }
-        // CAS UNE OU PLUSIEURS TRANSFORMATIONS AVEC PARAM
-        cv::RNG rng(time(0));
-        std::map<std::string, std::string> parsed_opts;
-        for (int i = 2; i < argc; ++i) {
-            std::string arg(argv[i]);
-            for (const auto& opt : poss_opt) {
-                // Vérifie que l'argument commence par opt
-                if (arg.rfind(opt, 0) == 0) {
-                    // Extrait la valeur après le '='
-                    std::string value = arg.substr(opt.size());
-                    // Stocke en mappant l'option à sa valeur
-                    parsed_opts[opt] = value;
-                    break; // passe à l'argument suivant
-                }
+    // CAS UNE OU PLUSIEURS TRANSFORMATIONS AVEC PARAM
+    cv::RNG rng(time(0));
+    std::map<std::string, std::string> parsed_opts;
+    for (int i = 2; i < argc; ++i) {
+        std::string arg(argv[i]);
+        for (const auto& opt : poss_opt) {
+            // Vérifie que l'argument commence par opt
+            if (arg.rfind(opt, 0) == 0) {
+                // Extrait la valeur après le '='
+                std::string value = arg.substr(opt.size());
+                // Stocke en mappant l'option à sa valeur
+                parsed_opts[opt] = value;
+                break; // passe à l'argument suivant
             }
         }
-
-        if (auto it = parsed_opts.find("-sp="); it != parsed_opts.end()) {
-            if (auto res = parse_sp(it->second)) {
-                // Structured binding pour décomposer le tuple
-                auto [salt, pepper] = *res;
-                add_salt_pepper_noise(img, rng, pepper, salt);
-            } else {
-                std::cerr << "Erreur dans le format de -sp= (attendu: salt, pepper)\n";
-                exit(1);
-            }
-        }
-        if (auto it = parsed_opts.find("-g="); it != parsed_opts.end()) {
-            if (auto res = parse_sp(it->second)) {
-                // Structured binding pour décomposer le tuple
-                auto [offset, dispersion] = *res;
-                add_gaussian_noise(img, rng, offset, dispersion);
-            } else {
-                std::cerr << "Erreur dans le format de -g= (attendu: offset, dispersion)\n";
-                exit(1);
-            }
-        }
-        if (auto it = parsed_opts.find("-cb="); it != parsed_opts.end()) {
-            if (auto res = parse_sp(it->second)) {
-                // Structured binding pour décomposer le tuple
-                auto [contrast, bright] = *res;
-                contrast_brightness_modifier(img, contrast, bright);
-            } else {
-                std::cerr << "Erreur dans le format de -cb= (attendu: contrast, bright)\n";
-                exit(1);
-            }
-        }
-
-        if (auto it = parsed_opts.find("-s="); it != parsed_opts.end()) {
-            if (auto res = parse_3(it->second)) {
-                // Structured binding pour décomposer le tuple
-                auto [nb_spot, min_radius, max_radius] = *res;
-                ajouterTaches(img, rng, nb_spot, min_radius, max_radius);
-            } else {
-                std::cerr << "Erreur dans le format de -s= (attendu: nb spot, min radius, max radius)\n";
-                exit(1);
-            }
-        }
-        auto it = parsed_opts.find("-r=");
-        if (it != parsed_opts.end()) {
-            int angle = std::stof(it->second); // conversion sans surcoût
-            rotate_img(angle);
-        }
-        if (auto it = parsed_opts.find("-t="); it != parsed_opts.end()) {
-            if (auto res = parse_sp(it->second)) {
-                // Structured binding pour décomposer le tuple
-                auto [dx, dy] = *res;
-                std::cout<<dx<<"  "<<dy<<std::endl;
-                translate_img(dx, dy);
-            } else {
-                std::cerr << "Erreur dans le format de -t= (attendu: dx, dy)\n";
-                exit(1);
-            }
-        }
-
-        // si seed not in params
     }
 
+    if (auto it = parsed_opts.find("-sp="); it != parsed_opts.end()) {
+        if (auto res = parse_sp(it->second)) {
+            // Structured binding pour décomposer le tuple
+            auto [salt, pepper] = *res;
+            add_salt_pepper_noise(img, rng, pepper, salt);
+        } else {
+            std::cerr << "Erreur dans le format de -sp= (attendu: salt, pepper)\n";
+            exit(1);
+        }
+    }
+    if (auto it = parsed_opts.find("-g="); it != parsed_opts.end()) {
+        if (auto res = parse_sp(it->second)) {
+            // Structured binding pour décomposer le tuple
+            auto [offset, dispersion] = *res;
+            add_gaussian_noise(img, rng, offset, dispersion);
+        } else {
+            std::cerr << "Erreur dans le format de -g= (attendu: offset, dispersion)\n";
+            exit(1);
+        }
+    }
+    if (auto it = parsed_opts.find("-cb="); it != parsed_opts.end()) {
+        if (auto res = parse_sp(it->second)) {
+            // Structured binding pour décomposer le tuple
+            auto [contrast, bright] = *res;
+            contrast_brightness_modifier(img, contrast, bright);
+        } else {
+            std::cerr << "Erreur dans le format de -cb= (attendu: contrast, bright)\n";
+            exit(1);
+        }
+    }
+
+    if (auto it = parsed_opts.find("-s="); it != parsed_opts.end()) {
+        if (auto res = parse_3(it->second)) {
+            // Structured binding pour décomposer le tuple
+            auto [nb_spot, min_radius, max_radius] = *res;
+            ajouterTaches(img, rng, nb_spot, min_radius, max_radius);
+        } else {
+            std::cerr << "Erreur dans le format de -s= (attendu: nb spot, min radius, max radius)\n";
+            exit(1);
+        }
+    }
+    auto it = parsed_opts.find("-r=");
+    if (it != parsed_opts.end()) {
+        int angle = std::stof(it->second); // conversion sans surcoût
+        rotate_img(angle);
+    }
+    if (auto it = parsed_opts.find("-t="); it != parsed_opts.end()) {
+        if (auto res = parse_sp(it->second)) {
+            // Structured binding pour décomposer le tuple
+            auto [dx, dy] = *res;
+            std::cout << dx << "  " << dy << std::endl;
+            translate_img(dx, dy);
+        } else {
+            std::cerr << "Erreur dans le format de -t= (attendu: dx, dy)\n";
+            exit(1);
+        }
+    }
+
+    // si seed not in params
+}
 
 int main(int argc, char const* argv[]) {
     if (argc < 2) {
@@ -267,7 +267,6 @@ int main(int argc, char const* argv[]) {
     std::string image_max_pepperth = argv[1];
     img = cv::imread(image_max_pepperth);
     cv::Mat calibrated_img = img.clone();
-    
     cv::Mat identity = cv::Mat::eye(3, 3, CV_32F);
     //  identity *= rotate_center(5, img.cols / 2, img.rows / 2);
     // print_mat(identity);
